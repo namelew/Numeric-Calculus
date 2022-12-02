6ec62a40c2378ebca4da18d2f698dee83e33d4ae
diogo <diogomaciel.cunha@gmail.com>
2022-12-01 21:32:30 -0300

    finish

--- linear_systens.py
+++ linear_systens.py
@@ -1,26 +1,32 @@
 from numpy.linalg import inv,norm
-from numpy import array, zeros, diag, diagflat, dot, tril, triu, append
+from numpy import array, zeros, diag, diagflat, dot, tril, triu, append, resize, identity
 
-def breakcase(a:array, b:array) -> float:
-    c = a - b
-    return dot(c,c)/dot(a,a)
+def breakcase(a) -> float:
+    s = 0
+    for i in range(len(a)):
+        s += a[i][0] ** 2
+    return s ** 0.5
 
 def jacobi(A,b,N=25,x=None,epsi=0.01):
-    # Create a vector of the diagonal elements of A
-    # and subtract them from A
-    D = diag(A)
-    R = A - diagflat(D)
-    # Iterate for N times 
-    prev = x
-    count = 0
-    for i in range(N):
-        x = (b - dot(R,x)) / D
-        if breakcase(x, prev) <= epsi:
-            break
-        prev = x
-        count += 1
+    F = []
+    d = []
 
-    return x,count
+    for i in range(len(A)) :
+        F.append(A[i]/A[i][i])
+        d.append(b[i]/A[i][i])
+
+    F = array(F)
+    d = array(d).reshape(-1,1)
+
+    x = zeros(d.shape)
+    n = 0
+
+    for _ in range(N):
+        x = (identity(F.shape[0])-F).dot(x) + d
+        if norm(A.dot(x)-b, 'fro') < epsi:
+            break
+        n+=1
+    return x, n
 
 def gauss_seidel(a:array,b:array,x=None,N=25,epsi=0.01):
     F = []
@@ -34,7 +40,6 @@ def gauss_seidel(a:array,b:array,x=None,N=25,epsi=0.01):
     F = array(F)
     d = array(d).reshape(-1,1)
     if x is None:
-        print(x)
         x = zeros(d.shape) 
     n = 0
 
@@ -42,11 +47,13 @@ def gauss_seidel(a:array,b:array,x=None,N=25,epsi=0.01):
     L = tril(F,-1) # Lower Triangle
     D = F-U-L # Main Diagonal
     LDI = inv(L + D)
+    prev = x
 
     for _ in range(N):
         x = LDI.dot(d) - LDI.dot(U).dot(x)
-        if norm(A.dot(x)-b) <= epsi:
+        if breakcase(A.dot(x)-b) < epsi:
             break
+        prev = x
         count+=1
     return x, count
 
